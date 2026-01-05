7f76a9eeaf5b547b69ccb3412e1fb736fa47c4d6
Ian Derrington <ian.derrington@gmail.com>
2026-01-05 14:25:32 -0800

    chore: add Jest configuration for TypeScript tests
    - Created jest.config.js with ts-jest preset
    - Added jest types to tsconfig.json
    - Fixed test runner configuration for publishing

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: {
+        jsx: 'react',
+        esModuleInterop: true,
+        allowSyntheticDefaultImports: true,
+        moduleResolution: 'node',
+      }
+    }]
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/__tests__/**',
+    '!src/**/*.test.{ts,tsx}',
+    '!src/**/index.ts'
+  ]
+};
+
